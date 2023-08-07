f24226d92a3fdad4213f5d802cc0f64fcb85e9df
hani100 <zz113111@naver.com>
2023-08-07 15:30:47 +0900

    mm

--- mainScript.js
+++ mainScript.js
@@ -1,29 +1,29 @@
 //javaScript
 //쿠키_popup
-let currentCookie=document.cookie;
-let cookieCheck=currentCookie.indexOf('green');
-console.log(cookieCheck)
+// let currentCookie=document.cookie;
+// let cookieCheck=currentCookie.indexOf('green');
+// console.log(cookieCheck)
 
-if(cookieCheck>-1){
-    document.querySelector('.notice').style.display="none";
-}else{
-    document.querySelector('.notice').style.display="block";
-}
+// if(cookieCheck>-1){
+//     document.querySelector('.notice').style.display="none";
+// }else{
+//     document.querySelector('.notice').style.display="block";
+// }
 
 
-document.querySelector('.hide').addEventListener('click',()=>{
-    let date=new Date();
-    date.setDate(date.getDate() + 7);
-    let setCookie = "CookieName=green;";
-        setCookie += "expires="+ date.toUTCString();
+// document.querySelector('.hide').addEventListener('click',()=>{
+//     let date=new Date();
+//     date.setDate(date.getDate() + 7);
+//     let setCookie = "CookieName=green;";
+//         setCookie += "expires="+ date.toUTCString();
 
-    document.cookie=setCookie;
-})
+//     document.cookie=setCookie;
+// })
 
-document.querySelector('.notice .close').addEventListener('click',function(){
-    console.log(this)
-    this.parentElement.style.display="none";
-})
+// document.querySelector('.notice .close').addEventListener('click',function(){
+//     console.log(this)
+//     this.parentElement.style.display="none";
+// })
 
 
 
